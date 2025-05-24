382386c91d720359083b25e57f027b1d7e05773a
miguelfinda <154908251+miguelfinda@users.noreply.github.com>
2025-05-24 16:46:52 +0100

    Add files via upload
    Subi o site InfoTech

---
+++ js/tratamento.js
@@ -0,0 +1,41 @@
+
+window.addEventListener("load", function (){
+const preloader=document.querySelector(".proce")
+
+const co=document.querySelector(".verde")
+
+setTimeout(()=>{
+
+preloader.style.display="none"
+
+co.style.display="block"
+
+
+
+}, 2000)
+
+
+})
+
+
+
+
+
+var btn = document.querySelector(".botao");
+var men = document.querySelector(".menu");
+
+
+btn.addEventListener("click", function () {
+  men.classList.toggle("show");
+
+  
+});
+
+
+
+
+
+
+
+
+
